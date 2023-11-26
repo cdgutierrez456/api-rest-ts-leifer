3bd5629675e52eb1dfeb81300ab3a72e66c7fc3b
Cristian Gutierrez <cdgutierrez456@gmail.com>
2023-11-26 13:29:40 -0500

    Haciendo conexion a base de datos mongo. Creando interfaz inicial de un carro. Creando las funciones CRUD de item controller

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,13 @@ import express from 'express';
 import cors from 'cors';
 
 import router from './routes'
+import db from './config/mongo'
 
 const PORT = process.env.PORT || 3000
 const app = express()
 app.use(cors())
 
 app.use(router)
+db().then(() => console.log('Conection ready'))
 
 app.listen(PORT, () => console.log(`Server running in port ${PORT}`));
---
+++ src/controllers/item.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express"
+
+export const getItem = (req: Request, res: Response) => {
+
+}
+
+export const getItems = (req: Request, res: Response) => {
+
+}
+
+export const updateItems = (req: Request, res: Response) => {
+
+}
+
+export const postItem = (req: Request, res: Response) => {
+
+}
+
+export const deleteItem = (req: Request, res: Response) => {
+
+}
