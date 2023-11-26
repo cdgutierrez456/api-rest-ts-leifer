922ba7eb15f9f7e27d70546995b5b8d415147f87
Cristian Gutierrez <cdgutierrez456@gmail.com>
2023-11-26 13:37:14 -0500

    Agregando middleware para poder resivir datos en tipo json en app.ts. Creando utilidad para responser errores cuando exista en las peticiones.

--- src/controllers/item.ts
+++ src/controllers/item.ts
@@ -1,21 +1,42 @@
 import { Request, Response } from "express"
+import { handleHttp } from "../utils/error.handler"
 
 export const getItem = (req: Request, res: Response) => {
-
+  try {
+    
+  } catch (e) {
+    handleHttp(res, 'ERROR_GET_ITEM')
+  }
 }
 
 export const getItems = (req: Request, res: Response) => {
-
+  try {
+    
+  } catch (e) {
+    handleHttp(res, 'ERROR_GET_ITEMS')
+  }
 }
 
 export const updateItems = (req: Request, res: Response) => {
-
+  try {
+    
+  } catch (e) {
+    handleHttp(res, 'ERROR_UPDATE_ITEM')
+  }
 }
 
 export const postItem = (req: Request, res: Response) => {
-
+  try {
+    
+  } catch (e) {
+    handleHttp(res, 'ERROR_CREATE_ITEM')
+  }
 }
 
 export const deleteItem = (req: Request, res: Response) => {
-
+  try {
+    
+  } catch (e) {
+    handleHttp(res, 'ERROR_DELETE_ITEM')
+  }
 }
