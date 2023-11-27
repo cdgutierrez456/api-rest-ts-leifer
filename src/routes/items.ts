e34c363c5f6c78b36039b26bc1c84aeb73689721
Cristian Gutierrez <cdgutierrez456@gmail.com>
2023-11-26 21:50:10 -0500

    Instalando bcrypt y jsonwebtoken para empezar pruebas de middleware de autenticacion

--- src/routes/items.ts
+++ src/routes/items.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
+
+import { logMiddleware } from "../middleware/log";
+
 import { getItem, getItems, postItem, deleteItem, updateItems } from "../controllers/item";
 
 const router = Router()
 
 router.get('/', getItems)
 
-router.get('/:id', getItem)
+router.get('/:id', logMiddleware, getItem)
 
 router.post('/', postItem)
 
